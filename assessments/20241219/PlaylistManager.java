e939ebed0a7f901eb01bdef88891b2456947a69d
git-student-sonabiju <sona.biju@edstem.com>
2024-12-25 20:39:25 +0530

    Update PlaylistManager.java

--- assessments/20241219/PlaylistManager.java
+++ assessments/20241219/PlaylistManager.java
@@ -1,15 +1,12 @@
-package com.ecld.java.assessment3;
-
+package com.ecld.java.assess;
 import java.util.Iterator;
+import java.util.List;
 
-public interface PlaylistManager {
-    void addTrack(Track track);
-
-    void removeTrack(Long trackId);
-
-    void moveTrack(int fromIndex, int toIndex);
-
-    Iterator<Track> shuffleIterator();
-
-    void updateRating(Long trackId, Integer newRating);
-}
+    public interface PlaylistManager {
+        void addTrack(Track track);
+        void removeTrack(Long trackId);
+        void moveTrack(int fromIndex, int toIndex);
+        Iterator<Track> shuffleIterator();
+        void updateRating(Long trackId, Integer newRating);
+        List<Track> getCurrentQueue();
+    }
